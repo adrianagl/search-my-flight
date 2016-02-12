1a2c227acfe552ca8d72505693618d6cf12940d2
Adriana Gomez <adrianagomez@paradigmatecnologico.com>
2016-02-12 18:42:03 +0100

    Include test for no available flight

--- src/test/java/com/flight/search/FlightSearchTest.java
+++ src/test/java/com/flight/search/FlightSearchTest.java
@@ -1,9 +1,11 @@
 package com.flight.search;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.flight.model.Airport;
 import com.flight.model.FlightSearchCriteria;
 import com.flight.utils.DateUtils;
 import static org.mockito.Mockito.mock;
@@ -11,9 +13,8 @@ import static org.mockito.Mockito.verify;
 
 public class FlightSearchTest
 {
-    private static final String ORIGIN = "CPH";
-    private static final String DESTINATION = "FRA";
-    private static final String INEXISTENT_AIRPORT = "OTHER";
+    private static final Airport ORIGIN = Airport.CPH;
+    private static final Airport DESTINATION = Airport.FRA;
     private static final Date TODAY = DateUtils.cleanDate(new Date());
 
     private FlightSearch flightSearch = new FlightSearch();
@@ -23,5 +24,13 @@ public class FlightSearchTest
         FlightSearchCriteria criteria = mock(FlightSearchCriteria.class);
         flightSearch.run(criteria);
         verify(criteria).validate();
+    }
+
+    @Test
+    public void runWhenNoFlightFoundThenReturnEmptyList() {
+        FlightSearchCriteria criteria = new FlightSearchCriteria(Airport.FCO, Airport.LHR, TODAY, 1, 1, 1);
+        List<SearchResult> results = flightSearch.run(criteria);
+
+        assertTrue(results.isEmpty());
     }
 }
